5d82e9fbfc82c8e63846ce1ce7fe22c93ae674bc
RekhuGopal <vrchinnarathod@gmail.com>
2023-02-10 19:44:52 +0530

    Updated

--- PythonScripts/triggerscripts.py
+++ PythonScripts/triggerscripts.py
@@ -27,7 +27,5 @@ def trigger_workflow(Workflow_Name,parameter1,parameter2):
 
       responseValue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
       print(responseValue.content)
-      responsevalue=requests.post(f"https://api.github.com/repos/{OWNER}/{REPO}/dispatches",json=data,headers=headers)
-      print("The respoinse message is ",responsevalue.content)
 
 trigger_workflow(Workflow_Name,parameter1,parameter2)
